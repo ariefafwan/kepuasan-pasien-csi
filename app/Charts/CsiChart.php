f28bff197b70c5bfb88824b02d0d82ad904552c5
ariefafwan <tmariefafwan@gmail.com>
2023-11-13 22:47:40 +0700

    chart fixing

--- app/Charts/CsiChart.php
+++ app/Charts/CsiChart.php
@@ -16,70 +16,79 @@ class CsiChart
 
     public function build(): \ArielMejiaDev\LarapexCharts\BarChart
     {
-        $dtahun = Hasil::select('tahun')->distinct()->get();
-        $tahun = [];
-        foreach ($dtahun as $key => $dta) {
-            $tahun[$key] = $dta->tahun;
-        }
+
         $mean = Hasil::select('pertanyaan_id', 'tahun')
             ->selectRaw("SUM(bobot_harapan) / COUNT(bobot_harapan) as mis")
             ->selectRaw("SUM(bobot_persepsi) / COUNT(bobot_persepsi) as mss")
             ->groupBy('pertanyaan_id', 'tahun')->get();
+        if (count($mean) >= 1) {
 
-        $summis = 0;
-        $summss = 0;
+            $dtahun = Hasil::select('tahun')->distinct()->get();
+            $tahun = [];
+            foreach ($dtahun as $key => $dta) {
+                $tahun[$key] = $dta->tahun;
+            }
 
-        foreach ($mean as $key => $m) {
-            $summis += $m->mis;
-            $summss += $m->mss;
-        }
+            $summis = 0;
+            $summss = 0;
 
-        $wf = [];
-        foreach ($mean as $key => $value) {
-            $wf[] = [
-                'pertanyaan_id' => $value->pertanyaan_id,
-                'tahun' => $value->tahun,
-                'wf' => $value->mis / $summis * 100,
-            ];
-        }
+            foreach ($mean as $key => $m) {
+                $summis += $m->mis;
+                $summss += $m->mss;
+            }
 
-        $hasil = [];
-        foreach ($wf as $key => $nwf) {
-            foreach ($mean as $key2 => $value2) {
-                if ($value2->pertanyaan_id == $nwf['pertanyaan_id'] && $value2->tahun == $nwf['tahun']) {
-                    $hasil[] = [
-                        'pertanyaan_id' => $nwf['pertanyaan_id'],
-                        'tahun' => $value2->tahun,
-                        'mis' => $value2->mis,
-                        'mss' => $value2->mss,
-                        'ws' => $nwf['wf'] * $value2->mss,
-                    ];
-                    // dd($nwf['pertanyaan_id']);
+            $wf = [];
+            foreach ($mean as $key => $value) {
+                $wf[] = [
+                    'pertanyaan_id' => $value->pertanyaan_id,
+                    'tahun' => $value->tahun,
+                    'wf' => $value->mis / $summis * 100,
+                ];
+            }
+
+            $hasil = [];
+            foreach ($wf as $key => $nwf) {
+                foreach ($mean as $key2 => $value2) {
+                    if ($value2->pertanyaan_id == $nwf['pertanyaan_id'] && $value2->tahun == $nwf['tahun']) {
+                        $hasil[] = [
+                            'pertanyaan_id' => $nwf['pertanyaan_id'],
+                            'tahun' => $value2->tahun,
+                            'mis' => $value2->mis,
+                            'mss' => $value2->mss,
+                            'ws' => $nwf['wf'] * $value2->mss,
+                        ];
+                        // dd($nwf['pertanyaan_id']);
+                    };
                 };
-            };
-        }
-
-        $wt = array_reduce($hasil, function ($carry, $item) {
-            if (!isset($carry[$item['tahun']])) {
-                $carry[$item['tahun']] = ['tahun' => $item['tahun'], 'ws' => $item['ws']];
-            } else {
-                $carry[$item['tahun']]['ws'] += $item['ws'];
             }
-            return $carry;
-        });
 
-        $csi = [];
-        foreach ($wt as $key => $value) {
-            foreach ($dtahun as $index => $hun) {
-                if ($hun->tahun == $value['tahun']) {
-                    $csi[] = $value['ws'] / 5;
+            $wt = array_reduce($hasil, function ($carry, $item) {
+                if (!isset($carry[$item['tahun']])) {
+                    $carry[$item['tahun']] = ['tahun' => $item['tahun'], 'ws' => $item['ws']];
+                } else {
+                    $carry[$item['tahun']]['ws'] += $item['ws'];
                 }
-            }
-        };
+                return $carry;
+            });
 
-        return $this->chart->barChart()
-            ->setTitle('Total CSI Pertahun.')
-            ->addData('Total CSI', $csi)
-            ->setXAxis($tahun);
+            $csi = [];
+            foreach ($wt as $key => $value) {
+                foreach ($dtahun as $index => $hun) {
+                    if ($hun->tahun == $value['tahun']) {
+                        $csi[] = $value['ws'] / 5;
+                    }
+                }
+            };
+            // dd($datapenduduk);
+            return $this->chart->barChart()
+                ->setTitle('Total CSI Pertahun.')
+                ->addData('Total CSI', $csi)
+                ->setXAxis($tahun);
+        } else {
+            return $this->chart->barChart()
+                ->setTitle('Total CSI Pertahun.')
+                ->addData('Total CSI', [''])
+                ->setXAxis(['']);
+        }
     }
 }
